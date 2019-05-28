6c334b028c0a94ffdae1be25240dbda62cdddd18
tzbailey <40056048+tzbailey@users.noreply.github.com>
2019-05-28 13:29:41 +0100

    updated docker repo AGAIN!

--- job-dsl/nodejsdocker.groovy
+++ job-dsl/nodejsdocker.groovy
@@ -14,7 +14,7 @@ job('NodeJS Docker example') {
     }
     steps {
         dockerBuildAndPublish {
-            repositoryName('tzbailey/jenkins-docker')
+            repositoryName('tzbailey/docker-jenkins')
             tag('${GIT_REVISION,length=9}')
             registryCredentials('dockerhub')
             forcePull(false)
